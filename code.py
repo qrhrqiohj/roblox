b16d8b0e127e21fdef6b0699e10bbfe5932f917a
Gruber450 <68507886+qrhrqiohj@users.noreply.github.com>
2025-06-19 02:55:25 +0100

    Update code.py

--- code.py
+++ code.py
@@ -72,7 +72,7 @@ def run(json_data, start_key, start_key2, addon, addon2, skip, game_pre, display
 
                         match sky_option:
                             case 1:
-                                addon = "0111487c7d3044bec56587c86740c9ce"
+                                addon = ["0111487c7d3044bec56587c86740c9ce", "aa50829c76ec2756b28883af05342ca6"]
                                 addon2 = "ffc3fc61237335e80db176e2827ed1ed"
                                 return json_data, start_key, start_key2, addon, addon2, skip, game_pre, display_names
                             case 2:
